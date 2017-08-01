e830c47e7b4ab1c9b2eee13de8b079f4970f2acf
Andy Parker <andyparker84@gmail.com>
2017-08-01 15:22:16 -0500

    daily project completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
 
+var app = express();
+
+app.listen(3000);
+app.use(express.static("assets"));
+app.use(bodyParser.json());
+
+app.get("/messages", function(req, res, next) {
+  res.status(200).json({ messages: messages });
+});
+
+const messages = ["hello", "call me", "see you soon"];
+
+app.post("/messages", function(req, res, next) {
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({ messages: messages });
+});
